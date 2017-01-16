58cba435aa2306ac73151eebc7b3f5342ef29b80
alvaromcarmena <alvaro.mcarmena@alumnos.upm.es>
2017-01-16 12:04:14 +0100

    Add files via upload

--- pox/stats_monitor.py
+++ pox/stats_monitor.py
@@ -1,5 +1,5 @@
-# Copyright 2013 Oscar Araque 
-#
+# author: 2013 Oscar Araque 
+# Version 2.0: Álvaro Martínez Carmena
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -38,6 +38,7 @@ from pox.lib.addresses import EthAddr, IPAddr # Address types
 import pox.lib.util as poxutil                # Various util functions
 import pox.lib.revent as revent               # Event library
 import pox.lib.recoco as recoco               # Multitasking library
+from pox.lib.util import dpid_to_str
 from pox.openflow.of_json import *
 
 import multiprocessing
@@ -45,6 +46,9 @@ import json
 
 # Create a logger for this component
 log = core.getLogger("Monitor")
+
+# Create a global var to portStatus method
+
 
 def _send_to_pipe(data):
     with open('/dev/shm/poxpipe','w') as pipe:
@@ -64,10 +68,12 @@ def _request_stats():
     log.info('Sending stats requests')
     for connection in core.openflow.connections:
         log.debug("Sending stats request")
-        connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
+        #connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
         connection.send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
+        #connection.send(of.ofp_stats_request(body=of.ofp_queue_stats_request()))
 
 def _handle_flowstats(event):
+    log.info(event.stats)
     stats = flow_stats_to_list(event.stats)
     dpid = poxutil.dpidToStr(event.connection.dpid)
     log.debug('Received flow stats from {}'.format(dpid))
@@ -79,6 +85,7 @@ def _handle_flowstats(event):
 
 def _handle_portstats(event):
     stats = flow_stats_to_list(event.stats)
+    #log.info(stats)
     dpid = poxutil.dpidToStr(event.connection.dpid)
     log.debug('Received port stats from {}'.format(dpid))
     data = {'type':"switch_portstats", "data":{'switch':dpid, 'stats':stats}}
@@ -94,6 +101,47 @@ def _handle_LinkEvent(event):
     data += '#'
     _to_pipe(data)
 
+def _handle_queuestats(event):
+    stats = flow_stats_to_list(event.stats)
+    log.info(stats)
+    dpid = poxutil.dpidToStr(event.connection.dpid)
+    log.debug('Received queue stats from {}'.format(dpid))
+    data = {'type':"switch_queuestats", "data":{'switch':dpid, 'stats':stats}}
+    data = json.dumps(data)
+    data += '#'
+    _to_pipe(data)
+
+def _handle_ConnectionDown (event):
+    log.info("Switch %s has come down.", dpid_to_str(event.dpid))
+    dpid = dpid_to_str(event.dpid)
+    data = {'type':"switch_down", "data":{'switch':dpid, 'down': True}}
+    data = json.dumps(data)
+    data += '#'
+    _to_pipe(data)
+
+
+def _handle_ConnectionUp (event):
+    log.info("Switch %s has come up.", dpid_to_str(event.dpid))
+    dpid = dpid_to_str(event.dpid)
+    data = {'type':"switch_down", "data":{'switch':dpid, 'down': False}}
+    data = json.dumps(data)
+    data += '#'
+    _to_pipe(data)
+
+def _handle_PortStatus (event):
+    if event.added:
+        action = "added"
+    elif event.deleted:
+        action = "removed"
+    else:       
+        action = "modified"
+        
+    dpid = dpid_to_str(event.dpid)
+    data = {'type':"port_status", "data":{'switch':dpid, 'portdown':{'port':event.port, 'action':action}}}
+    data = json.dumps(data)
+    data += '#'
+    _to_pipe(data)
+    print "Port %s on Switch %s has been %s." % (event.port, event.dpid, action)
 
 @poxutil.eval_args
 def launch (bar = False):
@@ -104,7 +152,10 @@ def launch (bar = False):
 
     core.addListenerByName("UpEvent", _go_up)
     core.openflow_discovery.addListenerByName("LinkEvent", _handle_LinkEvent)
-    core.openflow.addListenerByName("FlowStatsReceived", _handle_flowstats)
-    core.openflow.addListenerByName("PortStatsReceived", _handle_portstats)
-
+    #core.openflow.addListenerByName("FlowStatsReceived", _handle_flowstats)
+    core.openflow.addListenerByName("PortStatsReceived", _handle_portstats)    
+    #core.openflow.addListenerByName("QueueStatsReceived", _handle_queuestats)
+    core.openflow.addListenerByName("ConnectionDown", _handle_ConnectionDown)
+    core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
+    core.openflow.addListenerByName("PortStatus", _handle_PortStatus)
     recoco.Timer(7, _request_stats, recurring=True)
